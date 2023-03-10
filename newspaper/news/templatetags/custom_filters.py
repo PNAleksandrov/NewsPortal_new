9f66ba18933934263864bcd0c23c4370f67dc3d4
Pavel <paleksandrov@vk.com>
2023-03-10 13:09:04 +0300

    last version
    last version

---
+++ newspaper/news/templatetags/custom_filters.py
@@ -0,0 +1,15 @@
+from django import template
+
+
+register = template.Library()
+
+
+UNWANTED_WORDS = ['редиска', 'козел', 'урод', 'пидор']  # список нежелательных слов
+
+
+@register.filter(name='censor')
+def censor(text):
+    for word in UNWANTED_WORDS:
+        if word in text:
+            text = text.replace(word[1:len(word)], f'{len(word) * "*"}')
+    return text
